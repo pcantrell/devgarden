2a766eff1e936c8db9061c238ca704bd70fb3aec
Paul Cantrell <cantrell@pobox.com>
2016-02-17 00:41:06 -0600

    Pretty URL tweaks

--- app/helpers/pretty_url_helper.rb
+++ app/helpers/pretty_url_helper.rb
@@ -1,7 +1,9 @@
 module PrettyUrlHelper
 
   def pretty_url(url)
-    h(url.gsub(%r_^https?://(www\.)?_, ''))
+    h(url)
+      .gsub(%r_^https?://(www\.)?_, '')
+      .gsub(%r_/$_, '')
       .gsub(%r_^github\.com/_) { inline_icon_tag(:github) }
       .gsub(%r_^..\.wikipedia\.org/wiki/(.*)_) { inline_icon_tag(:wikipedia) + "Definition" }
       .html_safe

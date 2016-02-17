0f7cf6e13a3ea62ff734568acd4d6631235b692a
Paul Cantrell <cantrell@pobox.com>
2016-02-17 00:24:36 -0600

    Infinite scroll = infinite bugs

--- app/models/concerns/recent_scope.rb
+++ app/models/concerns/recent_scope.rb
@@ -2,7 +2,7 @@ module RecentScope
   extend ActiveSupport::Concern
 
   def scroll_continuation
-    { id: id, t: updated_at.to_f }
+    { id: id, t: updated_at.to_i, tusec: updated_at.usec.to_i }
   end
 
   included do
@@ -10,9 +10,11 @@ module RecentScope
       query = order('updated_at desc, id desc').limit(limit)
       if scroll_continuation
         before_id = scroll_continuation[:id]
-        before_time = Time.at(scroll_continuation[:t].to_f)
+        before_time = Time.at(
+          scroll_continuation[:t].to_i,
+          scroll_continuation[:tusec].to_i)
         query = query.where(
-          'updated_at <= ? or (updated_at = ? and id < ?)',
+          'updated_at < ? or (updated_at = ? and id < ?)',
           before_time,
           before_time,
           before_id)

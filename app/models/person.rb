22b4a248403c50a960ecb17eb072b1f8a7ff561f
Paul Cantrell <cantrell@pobox.com>
2016-02-20 22:15:04 -0600

    Starting in on form styling

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,6 +3,8 @@ class Person < ActiveRecord::Base
   has_many :projects, through: :participations
   has_many :role_offers, -> { includes(:role) }
 
+  validates :name, :email, presence: true
+
   include RecentScope
 
   def name
--- config/initializers/formtastic.rb
+++ config/initializers/formtastic.rb
@@ -1,2 +1,11 @@
 Formtastic::FormBuilder.action_class_finder = Formtastic::ActionClassFinder
 Formtastic::FormBuilder.input_class_finder = Formtastic::InputClassFinder
+
+Formtastic::FormBuilder.default_text_area_height = 1
+
+Formtastic::FormBuilder.required_string = ""
+Formtastic::FormBuilder.optional_string = "<abbr class='validation optional'>Optional</abbr>".html_safe
+
+Formtastic::FormBuilder.default_hint_class = "inline-hint"
+Formtastic::FormBuilder.default_inline_error_class = "inline-error"
+# Formtastic::FormBuilder.default_error_list_class = "errors"

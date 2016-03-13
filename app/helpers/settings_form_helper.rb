c8724e67a2c461b7904cb1d03167e9d428c4df57
Paul Cantrell <cantrell@pobox.com>
2016-03-12 23:33:45 -0600

    Genericized settings forms

--- app/helpers/settings_form_helper.rb
+++ app/helpers/settings_form_helper.rb
@@ -1,13 +1,13 @@
-module ProjectFormHelper
-  def project_form(tab_name, autosave: false, &block)
+module SettingsFormHelper
+  def settings_form_for(model, tab_name, autosave: false, &block)
     opts = {
       remote: autosave,
       html: {
-        id: "project-#{tab_name}",
+        id: "#{model.class.name.dasherize.downcase}-#{tab_name}",
         class: ('autosave' if autosave)
       }
     }
-    semantic_form_for(project, **opts) do |f|
+    semantic_form_for(model, **opts) do |f|
       capture do
         concat hidden_field_tag(:selected_tab, tab_name)
         yield f

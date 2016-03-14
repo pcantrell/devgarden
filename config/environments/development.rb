8266ffbc18af5c7b626ace53e32eb3730167c187
Paul Cantrell <cantrell@pobox.com>
2016-03-14 01:56:50 -0500

    Memcache!

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,8 @@ Rails.application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = !!ENV['cache']
+  config.cache_store = :dalli_store, 'localhost', { namespace: 'devgarden', expires_in: 24.hour, compress: true }
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false

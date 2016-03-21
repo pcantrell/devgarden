d21b2094914a910a6ec0cbffdf0a9f72e7e16e94
Paul Cantrell <cantrell@pobox.com>
2016-03-21 01:03:07 -0500

    Fixed job bugs when no associated job report

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -2,24 +2,25 @@ class ApplicationJob < ActiveJob::Base
 
   around_perform do |job, block|
     @job_report = job.arguments.first[:job_report]
-    return block.call unless @job_report
 
     begin
-      @job_report.results = block.call
+      results = block.call
+      @job_report&.results = results
     rescue => e
-      @job_report.error = {
+      @job_report&.error = {
         message: e.message,
         backtrace: e.backtrace
       }
+      raise
     ensure
-      @job_report.completed_at = Time.now
-      @job_report.save!
+      @job_report&.completed_at = Time.now
+      @job_report&.save!
     end
   end
 
   def show_message(message)
     logger.info message
-    @job_report.update!(message: message) if @job_report
+    @job_report&.update!(message: message) if @job_report
   end
 
 end

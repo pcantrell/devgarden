29d942d1f2bddbcb01b8a27347cd1d3a51e60638
Paul Cantrell <cantrell@pobox.com>
2016-03-27 00:35:37 -0500

    Also filter secret key base

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password, /^DEV_GARDEN_/]
+Rails.application.config.filter_parameters += [:password, /^DEV_GARDEN_/, /secret/i]

113e106bb2317bf701b5635cd1cbb3a9151e31a7
Paul Cantrell <cantrell@pobox.com>
2016-04-07 23:33:45 -0500

    Don’t retry jobs with job reports. Fixes #68

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -7,11 +7,16 @@ class ApplicationJob < ActiveJob::Base
       results = block.call
       @job_report&.results = results
     rescue => e
-      @job_report&.error = {
-        message: e.message,
-        backtrace: e.backtrace
-      }
-      raise
+      if @job_report
+        # If there is a report, user can see error & decode to retry manually
+        @job_report.error = {
+          message: e.message,
+          backtrace: e.backtrace
+        }
+      else
+        # No report, so automatically retry
+        raise
+      end
     ensure
       @job_report&.completed_at = Time.now
       @job_report&.save!
